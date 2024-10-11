f3c994968be1f81693934f69598cfa31157cd845
ndelanhese <nnathanh3@gmail.com>
2024-10-11 07:43:09 -0300

    feat: add coordinate validation

--- domain/application/get-many-properties.service.go
+++ domain/application/get-many-properties.service.go
@@ -6,18 +6,38 @@ import (
 	"gorm.io/gorm"
 )
 
-type GetManyPropertiesService struct {
-	SearchFilter *string
-	Database     *gorm.DB
+type GetManyPropertiesFilters struct {
+	Search    *string
+	Latitude  *float64
+	Longitude *float64
 }
 
-func (self *GetManyPropertiesService) Execute() (*[]entities.Property, error) {
+type GetManyPropertiesService struct {
+	Database *gorm.DB
+}
+
+func (self *GetManyPropertiesService) Execute(filters GetManyPropertiesFilters) (*[]entities.Property, error) {
 	var properties []entities.Property
 	query := self.Database.Model(&entities.Property{})
 
-	if *self.SearchFilter != "" {
-		query = query.Where("details ILIKE ?", "%"+*self.SearchFilter+"%").
-			Or("address ILIKE ?", "%"+*self.SearchFilter+"%")
+	if filters.Search != nil {
+		query = query.Where("details ILIKE ?", "%"+*filters.Search+"%").
+			Or("address ILIKE ?", "%"+*filters.Search+"%")
+	}
+
+	if filters.Latitude != nil && filters.Longitude != nil {
+		latitude := *filters.Latitude
+		longitude := *filters.Longitude
+
+		haversineQuery := `
+			6371 * acos(
+				cos(radians(?)) * cos(radians(latitude)) * 
+				cos(radians(longitude) - radians(?)) + 
+				sin(radians(?)) * sin(radians(latitude))
+			) <= 5
+		`
+
+		query = query.Where(gorm.Expr(haversineQuery, latitude, longitude, latitude))
 	}
 
 	query = query.Where("deleted_at IS NULL")
