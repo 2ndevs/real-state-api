725e9af3f6affeed6fc9915c09ae352dd8ce0c59
ndelanhese <nnathanh3@gmail.com>
2024-10-16 08:45:27 -0300

    feat: add find many properties filters

--- domain/application/get-many-properties.service.go
+++ domain/application/get-many-properties.service.go
@@ -7,9 +7,17 @@ import (
 )
 
 type GetManyPropertiesFilters struct {
-	Search    *string
-	Latitude  *float64
-	Longitude *float64
+	Search         *string
+	Latitude       *float64
+	Longitude      *float64
+	IsNew          *bool
+	WithDiscount   *bool
+	IsApartment    *bool
+	RecentlySold   *bool
+	RecentlyBuilt  *bool
+	MostVisited    *bool
+	AllowFinancing *bool
+	IsSpecial      *bool
 }
 
 type GetManyPropertiesService struct {
@@ -40,7 +48,29 @@ func (self *GetManyPropertiesService) Execute(filters GetManyPropertiesFilters) 
 		query = query.Where(gorm.Expr(haversineQuery, latitude, longitude, latitude))
 	}
 
+	if filters.IsNew != nil {
+		query = query.Order("created_at DESC")
+	}
+
+	if filters.WithDiscount != nil {
+		query = query.Where("discount > 0")
+	}
+
+	if filters.RecentlySold != nil {
+		query = query.Where("is_sold = true").Order("updated_at DESC")
+	}
+
+	if filters.RecentlyBuilt != nil {
+		query = query.Order("construction_year DESC")
+	}
+
+	if filters.IsSpecial != nil {
+		query = query.Where("is_highlight = true").Order("updated_at DESC")
+	}
+
 	query = query.Where("deleted_at IS NULL")
+
+	// TODO -> add associations filters
 
 	getPropertiesTransaction := query.Find(&properties)
 
