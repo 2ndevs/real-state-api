8abafa16144550fe0fce3c9c914c90b4321dbea3
ndelanhese <nnathanh3@gmail.com>
2024-10-17 17:50:22 -0300

    fix: change get search params to presenter

--- infra/http/routes/internals/controllers/get-many-properties.controller.go
+++ infra/http/routes/internals/controllers/get-many-properties.controller.go
@@ -6,8 +6,6 @@ import (
 	"main/domain/application"
 	"main/infra/http/middlewares"
 	"main/infra/http/routes/internals/presenters"
-	"main/utils"
-	"main/utils/libs"
 	"net/http"
 )
 
@@ -22,37 +20,7 @@ func GetManyProperties(write http.ResponseWriter, request *http.Request) {
 
 	propertiesService := application.GetManyPropertiesService{Database: database}
 
-	searchFilter := request.URL.Query().Get("search")
-	latitudeFilter := request.URL.Query().Get("latitude")
-	longitudeFilter := request.URL.Query().Get("longitude")
-	isNewFilter := request.URL.Query().Get("is_new")
-	withDiscountFilter := request.URL.Query().Get("with_discount")
-	recentlySold := request.URL.Query().Get("recently_sold")
-	recentlyBuilt := request.URL.Query().Get("recently_built")
-	isSpecial := request.URL.Query().Get("is_special")
-	isApartment := request.URL.Query().Get("is_apartment")
-	allowFinancing := request.URL.Query().Get("allow_financing")
-
-	filters := application.GetManyPropertiesFilters{}
-
-	if searchFilter != "" {
-		filters.Search = &searchFilter
-	}
-
-	latitude := libs.ValidateAndConvertCoordinate(latitudeFilter, -90, 90)
-	longitude := libs.ValidateAndConvertCoordinate(longitudeFilter, -180, 180)
-	if latitude != nil && longitude != nil {
-		filters.Latitude = latitude
-		filters.Longitude = longitude
-	}
-
-	filters.IsNew = utils.ParseParamToBool(isNewFilter)
-	filters.WithDiscount = utils.ParseParamToBool(withDiscountFilter)
-	filters.RecentlySold = utils.ParseParamToBool(recentlySold)
-	filters.RecentlyBuilt = utils.ParseParamToBool(recentlyBuilt)
-	filters.IsSpecial = utils.ParseParamToBool(isSpecial)
-	filters.IsApartment = utils.ParseParamToBool(isApartment)
-	filters.AllowFinancing = utils.ParseParamToBool(allowFinancing)
+	filters := httpPresenter.GetSearchParams(request)
 
 	properties, getPropertiesErr := propertiesService.Execute(filters)
 	if getPropertiesErr != nil {
@@ -68,7 +36,6 @@ func GetManyProperties(write http.ResponseWriter, request *http.Request) {
 
 	write.WriteHeader(http.StatusCreated)
 	err := json.NewEncoder(write).Encode(response)
-
 	if err != nil {
 		core.HandleHTTPStatus(write, err)
 	}
